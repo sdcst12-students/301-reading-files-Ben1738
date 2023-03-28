43d164dae1cd816a71a6f967c87a843da681f632
Ben1738 <benh668@deltalearns.ca>
2023-03-28 11:33:30 -0700

    s

--- task2.py
+++ task2.py
@@ -4,3 +4,33 @@ The data from this file contains 3 numbers on each line.  Determine how many lin
 Pythagorean triples are numbers where all of the sides are integers, and the 3 sides form a right triangle.
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
+
+filenameA = 'task02a.txt'
+filenameB = 'task02b.txt'
+filenameC = 'task02c.txt'
+
+fileA = open(filenameA, 'r')
+fileB = open(filenameB, 'r')
+fileC = open(filenameC, 'r')
+
+dataA = fileA.read()
+dataB = fileB.read()
+dataC = fileC.read()
+
+listA = dataA.split('\n')
+listB = dataB.split('\n')
+listC = dataC.split('\n')
+
+print(f'{listA}\n{listB}\n{listC}')
+
+for i in enumerate(listA):
+    if listA[int(i[1])]==''or listA[int(i[((i[0])+1)])]=='': 
+    #or listA[(i[0]+1)[1]]=='' or listA[(i[0]+2)[1]]=='':
+        print('ur agy')
+    else:
+        a=int(i[1])+1
+        b=int(i[1])+2
+        c=int(i[1])
+        if int(listA[c])**2+int(listA[a])**2==int(listA[b])**2 or int(listA[a])**2+int(listA[b])**2==int(listA[c])**2 or int(listA[c])**2+int(listA[b])**2==int(listA[a])**2:
+            print('borgir')
+
