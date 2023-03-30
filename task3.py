d41306292a217f4024a01b17bd4825ca20943755
Ben1738 <benh668@deltalearns.ca>
2023-03-30 11:03:11 -0700

    h
    h

--- task3.py
+++ task3.py
@@ -7,3 +7,32 @@ Read the contents of task03.txt into your program and determine the points value
 
 For sample data task03.txt, the largest sum should be 68787
 """
+
+filename = 'task03.txt'
+file = open(filename, 'r')
+data = file.read()
+list = data.split('\n')
+
+numList = []
+numList2 = []
+nums = []
+
+for i in enumerate(list):
+    numList.append(i[1])
+    if i[1] is '':
+        numList2 = numList
+        numList2.remove('')
+        numList2 = [int(j) for j in numList2]
+        print(sum(numList2))
+        nums.append(sum(numList2))
+        numList.clear
+        numList2.clear
+    else:
+        print(numList)
+    if numList==['','']:
+        print(nums)
+        nums.sort()
+        bigNum = nums[-1]
+        print(bigNum)
+        break
+
